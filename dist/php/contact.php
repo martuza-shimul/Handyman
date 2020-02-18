731c925daeb4fe8ed0fc6ecc8e4fcfacefcae693
Martuza <martuza.shimul@gmail.com>
2020-02-19 05:18:57 +0600

    php files updated

--- dist/php/contact.php
+++ dist/php/contact.php
@@ -18,17 +18,17 @@ if(empty($_POST['name'])  		||
 $name= $_POST['name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
-$lastname = $_POST['postcode'];
+$postcode = $_POST['postcode'];
 $message = $_POST['message'];
 
 	
 // Create the email and send the message
 
 $to = 'tripkeys@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Quote Engine:  $name";
-$email_body = "You have received a new message from your Painter London website Quote Engine .\n\n"."Here are the details:\n\nFirst Name: $name\n\n Last Name: $lastname \n\nPhone: $phone\n\nEmail: $email\n\n  \n Message: $message ";
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your Handyman London website contact form .\n\n"."Here are the details:\n\nFirst Name: $name\n\n Postcode: $postcode \n\n Phone: $phone\n\nEmail: $email\n\n  \n Message: $message ";
 
-$headers = "From: noreply@turkish-citizenship.co.uk\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@handyman-london.co.uk\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 
 $headers .= "Reply-To: $email";	
 mail($to,$email_subject,$email_body,$headers);
--- dist/php/data.php
+++ dist/php/data.php
@@ -3,7 +3,7 @@
 if(empty($_POST['first-name'])  		||
    empty($_POST['email'])	||
    empty($_POST['phone']) 		||
-   empty($_POST['last-name'])	||
+   empty($_POST['postcode'])	||
    empty($_POST['message']) 		||
    
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
@@ -13,13 +13,13 @@ if(empty($_POST['first-name'])  		||
    }
 
    
-$fuel_type = $_POST['first'];
+$service_type = $_POST['first'];
 // $second = $_POST['check'];
 // $type = implode(', ', $second); //vehicle type
-// $price_range = $_POST['second'];
-// $area = $_POST['type']; // in which area
-// $urgency = $_POST['third']; //timeline
-// $timeline = $_POST['fourth']; //material purchase
+$service_name = $_POST['second'];
+//$area = $_POST['type']; // in which area
+$urgency = $_POST['third']; //timeline
+$materials = $_POST['fourth']; //material purchase
 
 
 // if(!empty($second)) {
@@ -33,7 +33,7 @@ $fuel_type = $_POST['first'];
 $first_name = $_POST['first-name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
-$lastname = $_POST['last-name'];
+$postcode = $_POST['postcode'];
 $message = $_POST['message'];
 
 	
@@ -42,13 +42,13 @@ $message = $_POST['message'];
 $to = 'tripkeys@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Quote Engine:  $first_name";
 
-$email_body = "You have received a new message from your Car Hire website Quote Engine .\n\n"."Here are the details:\n\nFirst Name: $first_name\n\n  Last Name: $lastname \n\nPhone: $phone\n\nEmail: $email\n\n Message: $message
+$email_body = "You have received a new message from your Handyman website Quote Engine .\n\n"."Here are the details:\n\nFirst Name: $first_name\n\n  Postcode: $postcode \n\nPhone: $phone\n\nEmail: $email\n\n Message: $message
 
-\n\n\n Here is the Quote Request: \n type of Query: $fuel_type\n ";
+\n\n\n Here is the Quote Request: \n Place to service: $service_type\n Service Name: $service_name\n Urgency: $urgency\n Materials: $materials";
 
 // Weekly Price range: $price_range\n Area: $area \n How urgent : $urgency \n  For how long: $timeline 
 
-$headers = "From: noreply@turkish-citizenship.co.uk\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@handyman-london.co.uk\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 
 $headers .= "Reply-To: $email";	
 mail($to,$email_subject,$email_body,$headers);
