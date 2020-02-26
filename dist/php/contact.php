350c33fbf1ab588c44432c0e76995b2923240955
Martuza <martuza.shimul@gmail.com>
2020-02-26 21:48:46 +0600

    ver 1.1.0

--- dist/php/contact.php
+++ dist/php/contact.php
@@ -15,20 +15,26 @@ if(empty($_POST['name'])  		   ||
 
    // File upload functionality
 $new_path = "No files Uploaded";
+$counter = 0;
 if(!empty($_FILES['uploaded_file']))
   {
     $path = "uploads/";
     $path = $path . basename( $_FILES['uploaded_file']['name']);
 
     if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
-      $new_path: "File:  <a href=" .$path. ">Check here</a>";
+    //   $new_path = "<a href=" .$path. ">Check here</a>";
+    //   $new_path = "<a href=" echo $path ">Check here</a>";
+      $new_path = "https://handymanco.co.uk/dist/php/".$path;
+    //   $counter = 1;
   
     } else{
         echo "There was an error uploading the file, please try again!";
     }
   }
 
-   
+// if(counter === 1){
+    
+// }   
 
 
 $name= $_POST['name'];
@@ -42,11 +48,12 @@ $message = $_POST['message'];
 
 $to = 'tripkeys@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your Handyman London website contact form .\n\n"."Here are the details:\n\nFirst Name: $name\n\n Postcode: $postcode \n\n Phone: $phone\n\nEmail: $email\n\n  \n Message: $message \n Uploaded File: $new_path";
+$email_body = "You have received a new message from your Handyman London website contact form .\n\n"."Here are the details:\n\nFirst Name: $name\n\n Postcode: $postcode \n\n Phone: $phone\n\nEmail: $email\n  \n Message: $message \n Uploaded File:" .$new_path ;
+
 // if(!empty($new_path)){
-//    echo $new_path;
+//   "<a href=" .new_$path. ">Check here</a>"
 // }else{
-//    echo "No files uploaded";
+//     $new_path = "No files Uploaded";
 // };
 $headers = "From: noreply@handyman-london.co.uk\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 
--- dist/php/data.php
+++ dist/php/data.php
@@ -14,13 +14,17 @@ if(empty($_POST['first-name'])  		||
 
    // File upload functionality
 $new_path = "No files Uploaded";
+$counter = 0;
 if(!empty($_FILES['uploaded_file']))
   {
     $path = "uploads/";
     $path = $path . basename( $_FILES['uploaded_file']['name']);
 
     if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $path)) {
-      $new_path: "File:  <a href=" .$path. ">Check here</a>";
+    //   $new_path = "<a href=" .$path. ">Check here</a>";
+    //   $new_path = "<a href=" echo $path ">Check here</a>";
+      $new_path = "https://handymanco.co.uk/dist/php/".$path;
+    //   $counter = 1;
   
     } else{
         echo "There was an error uploading the file, please try again!";
@@ -59,13 +63,13 @@ $message = $_POST['message'];
 $to = 'tripkeys@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Quote Engine:  $first_name";
 
-$email_body = "You have received a new message from your Handyman website Quote Engine .\n\n"."Here are the details:\n\nFirst Name: $first_name\n\n  Postcode: $postcode \n\nPhone: $phone\n\nEmail: $email\n\n Message: $message
+$email_body = "You have received a new message from your Handyman website Quote Engine .\n\n"."Here are the details:\n\nFirst Name: $first_name\n Postcode: $postcode \nPhone: $phone\nEmail: $email\n Message: $message
 
-\n\n\n Here is the Quote Request: \n Place to service: $service_type\n Service Name: $service_name\n Urgency: $urgency\n Materials: $materials\n Uploaded File: $new_path";
+\n\n\n Here is the Quote Request: \n Place to service: $service_type\n Service Name: $service_name\n Urgency: $urgency\n Materials: $materials\n Uploaded File: " .$new_path;
 
 // Weekly Price range: $price_range\n Area: $area \n How urgent : $urgency \n  For how long: $timeline 
 
-$headers = "From: noreply@handyman-london.co.uk\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@handymanco.co.uk\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 
 $headers .= "Reply-To: $email";	
 mail($to,$email_subject,$email_body,$headers);
